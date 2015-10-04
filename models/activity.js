844ebedf8a9438b1ce3c6fd41f190a809cb138a6
kathrynsiegel <ksiegel@mit.edu>
2015-10-04 13:56:05 -0400

    added model file

---
+++ models/activity.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+
+var activitySchema = mongoose.Schema({
+  /* Exercise 1: Declare schema here */
+});
+
+// When we 'require' this model in another file (e.g. routes),
+// we specify what we are importing form this file via module.exports.
+// Here, we are 'exporting' the mongoose model object created from
+// the specified schema.
+module.exports = mongoose.model("Activity", activitySchema);
