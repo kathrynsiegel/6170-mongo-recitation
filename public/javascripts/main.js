188ca03f898551dda23fd202b4cba948ec3bd6d6
kathrynsiegel <ksiegel@mit.edu>
2015-10-04 17:47:06 -0400

    added more javascript for remote adding obj

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,28 +1,19 @@
 $(document).ready(function() {
-  $('#submit-login-button').click(function() {
-    $.post('/login',
-            {username: $('#kerberosInput').val(),
-             password: $('#passwordInput').val()},
+  $('#submit-button').click(function() {
+    $.post('/', {
+        activity: $('#type-input').val(),
+        duration: $('#minutes-input').val(),
+        intensity: "asdf"
+    },
     function(resp, status, jQxhr) {
       if (resp.success) {
-        location.reload();
-      } else {
-        $('#alert-box').text(resp.message);
-        $('#alert-box').css('display', 'block');
-      }
-    }).fail(function(jqXhr, status, err) {
-      console.log(err);
-    });
-  });
-  $('#submit-email-button').click(function() {
-    $.post('/forgot', {kerberos: $('#kerbInput').val()},
-    function(resp, status, jQxhr) {
-      if (resp.success) {
+        console.log("yay");
         $('#success-box').text(resp.message);
         $('#success-box').css('display', 'block');
         $('#alert-box').text('');
         $('#alert-box').css('display', 'none');
       } else {
+        console.log("boo");
         $('#alert-box').text(resp.message);
         $('#alert-box').css('display', 'block');
         $('#success-box').text('');
@@ -31,45 +22,5 @@ $(document).ready(function() {
     }).fail(function(jqXhr, status, err) {
       console.log(err);
     });
-  });
-  $('#submit-hours-button').click(function() {
-    $.post('/hours',
-            {hours: $('#hoursInput').val(),
-             supervisor: $('#supervisor-dropdown').val(),
-             description: $('#descriptionInput').val()},
-    function(resp, status, jQxhr) {
-      if (resp.success) {
-        location.reload();
-      } else {
-        $('#alert-box').text(resp.message);
-        $('#alert-box').css('display', 'block');
-      }
-    }).fail(function(jqXhr, status, err) {
-      console.log(err);
-    });
-  });
-  $('#submit-new-password-button').click(function() {
-    var url = window.location.href;
-    $.post('/reset/' + url.substr(url.lastIndexOf("/") + 1),
-            {password: $('#newPasswordInput').val()},
-    function(resp, status, jQxhr) {
-      if (resp.success) {
-        $('#alert-box').text('');
-        $('#alert-box').css('display','none');
-        window.location = "/";
-      } else {
-        $('#alert-box').text(resp.message);
-        $('#alert-box').css('display', 'block');
-        $('#success-box').text('');
-        $('#success-box').css('display', 'none');
-      }
-    }).fail(function(jqXhr, status, err) {
-      console.log(err);
-    });
-  });
-
-  $('#supervisorInput li a').click(function() {
-    $('#supervisor-dropdown:first-child').text($(this).text());
-    $('#supervisor-dropdown:first-child').val($(this).text());
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/activitylog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { activities: db.activities });
+});
+
+router.post('/', function(req, res, next) {
+  activity = req.body;
+  console.log("activity: " + activity);
+  db.activities.push(activity);
+  console.log("activities: " + db.activities);
+  res.send({success: true, message: "Success!"});
 });
 
 module.exports = router;
