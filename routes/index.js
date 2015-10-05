84a880a96db6f8ca0a9fd24ccb5b29a91812b22a
kathrynsiegel <ksiegel@mit.edu>
2015-10-04 21:09:41 -0400

    part2 done

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/activitylog');
+var Activity = require('../models/activity');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { activities: db.activities });
+  Activity.find(function(err, activitiesList) {
+    if (err) console.log(err);
+    res.render('index', {activities: activitiesList});
+  });
 });
 
 router.post('/', function(req, res, next) {
-  activity = req.body;
-  console.log("activity: " + activity);
-  db.activities.push(activity);
-  console.log("activities: " + db.activities);
   res.send({success: true, message: "Success!"});
 });
 
